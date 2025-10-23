38f9c83170fd879aabdd90518c448f33798a6ff6
Anubhav Mandarwal <socialengineerhundred@gmail.com>
2025-10-23 12:13:39 +0530

    Removing quick verification script

--- src/quick_verification.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-raw = pd.read_csv("/Users/curious_techie/Desktop/predictive-maintainence/raw/data/HPC_2k.log_structured.csv")
-feat = pd.read_csv("/Users/curious_techie/Desktop/predictive-maintainence/raw/data/hpc_features.csv")
-# Suppose you exported LineId to hpc_features.csv
-merged = raw.merge(feat, left_on="LineId", right_on="LineId")
-# Compare anomaly columns
-anomalies_mismatch = merged[merged["anomaly_x"] != merged["anomaly_y"]]
-print(anomalies_mismatch)
